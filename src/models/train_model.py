924eefce6398a3ed8b30a98a65f158ce8da738f2
imahrous13 <imahrous13>
2025-08-10 17:30:25 +0300

    Add LearningAlgorithms model and enhance training logic in train_model.py

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -70,6 +70,19 @@ selected_features, ordered_features, ordered_scores = (
     learning_algorithms.forward_selection(max_features, X_train, y_train)
 )
 
+selected_features = [
+    "pca_1",
+    "duration",
+    "gyr_r_freq_0.0_Hz_ws_14",
+    "pca_3",
+    "acc_z_freq_1.786_Hz_ws_14",
+    "gyr_r_freq_0.714_Hz_ws_14",
+    "gyr_x_freq_0.0_Hz_ws_14",
+    "gyr_r",
+    "gyr_z_max_freq",
+    "gyr_r_freq_weighted",
+]
+
 plt.figure(figsize=(10, 5))
 plt.plot(np.arange(1, max_features + 1, 1), ordered_scores)
 plt.xticks("Number of features")
@@ -79,22 +92,173 @@ plt.show()
 # --------------------------------------------------------------
 # Grid search for best hyperparameters and model selection
 # --------------------------------------------------------------
+possible_feature_sets = [
+    feature_set_1,
+    feature_set_2,
+    feature_Set_3,
+    feature_set_4,
+    selected_features,
+]
 
+feature_names = [
+    "feature_set_1",
+    "feature_set_2",
+    "feature_set_3",
+    "feature_set_4",
+    "selected_features",
+]
+
+iterations = 1
+score_df = pd.DataFrame()
+
+
+learner = ClassificationAlgorithms()
+
+for i, f in zip(range(len(possible_feature_sets)), feature_names):
+    print("Feature set:", i)
+    selected_train_X = X_train[possible_feature_sets[i]]
+    selected_test_X = X_test[possible_feature_sets[i]]
+
+    # First run non deterministic classifiers to average their score.
+    performance_test_nn = 0
+    performance_test_rf = 0
+
+    for it in range(0, iterations):
+        print("\tTraining neural network,", it)
+        (
+            class_train_y,
+            class_test_y,
+            class_train_prob_y,
+            class_test_prob_y,
+        ) = learner.feedforward_neural_network(
+            selected_train_X,
+            y_train,
+            selected_test_X,
+            gridsearch=False,
+        )
+        performance_test_nn += accuracy_score(y_test, class_test_y)
+
+        print("\tTraining random forest,", it)
+        (
+            class_train_y,
+            class_test_y,
+            class_train_prob_y,
+            class_test_prob_y,
+        ) = learner.random_forest(
+            selected_train_X, y_train, selected_test_X, gridsearch=True
+        )
+        performance_test_rf += accuracy_score(y_test, class_test_y)
+
+    performance_test_nn = performance_test_nn / iterations
+    performance_test_rf = performance_test_rf / iterations
+
+    # And we run our deterministic classifiers:
+    print("\tTraining KNN")
+    (
+        class_train_y,
+        class_test_y,
+        class_train_prob_y,
+        class_test_prob_y,
+    ) = learner.k_nearest_neighbor(
+        selected_train_X, y_train, selected_test_X, gridsearch=True
+    )
+    performance_test_knn = accuracy_score(y_test, class_test_y)
+
+    print("\tTraining decision tree")
+    (
+        class_train_y,
+        class_test_y,
+        class_train_prob_y,
+        class_test_prob_y,
+    ) = learner.decision_tree(
+        selected_train_X, y_train, selected_test_X, gridsearch=True
+    )
+    performance_test_dt = accuracy_score(y_test, class_test_y)
+
+    print("\tTraining naive bayes")
+    (
+        class_train_y,
+        class_test_y,
+        class_train_prob_y,
+        class_test_prob_y,
+    ) = learner.naive_bayes(selected_train_X, y_train, selected_test_X)
+
+    performance_test_nb = accuracy_score(y_test, class_test_y)
+
+    # Save results to dataframe
+    models = ["NN", "RF", "KNN", "DT", "NB"]
+    new_scores = pd.DataFrame(
+        {
+            "model": models,
+            "feature_set": f,
+            "accuracy": [
+                performance_test_nn,
+                performance_test_rf,
+                performance_test_knn,
+                performance_test_dt,
+                performance_test_nb,
+            ],
+        }
+    )
+    score_df = pd.concat([score_df, new_scores])
 
 # --------------------------------------------------------------
 # Create a grouped bar plot to compare the results
 # --------------------------------------------------------------
 
+score_df.sort_values(by="accuracy", ascending=False)
 
+plt.figure(figsize=(10, 10))
+sns.barplot(x="model", y="accuracy", hue="feature_set", data=score_df)
+plt.xlabel("Model")
+plt.ylabel("Accuracy")
+plt.ylim(0.7, 1.0)
+plt.legend(loc="lower right")
+plt.show()
 # --------------------------------------------------------------
 # Select best model and evaluate results
 # --------------------------------------------------------------
+(
+    class_train_y,
+    class_test_y,
+    class_train_prob_y,
+    class_test_prob_y,
+) = learner.random_forest(
+    X_train[feature_set_4], y_train, X_test[feature_set_4], gridsearch=True
+)
 
+accuracy = accuracy_score(y_test, class_test_y)
 
+classes = class_test_prob_y.columns
+cm = confusion_matrix(y_test, class_test_y, labels=classes)
+
+# create confusion matrix for cm
+plt.figure(figsize=(10, 10))
+plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+plt.title("Confusion matrix")
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes, rotation=45)
+plt.yticks(tick_marks, classes)
+
+thresh = cm.max() / 2.0
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(
+        j,
+        i,
+        format(cm[i, j]),
+        horizontalalignment="center",
+        color="white" if cm[i, j] > thresh else "black",
+    )
+plt.ylabel("True label")
+plt.xlabel("Predicted label")
+plt.grid(False)
+plt.show()
 # --------------------------------------------------------------
 # Select train and test data based on participant
 # --------------------------------------------------------------
-
+participant_df = df.drop(["set", "category"], axis=1)
+X_train = participant_df[participant_df["participant"] != "A"].drop("label", axis=1)
 
 # --------------------------------------------------------------
 # Use best model again and evaluate results
